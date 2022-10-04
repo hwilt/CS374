540bb1494b99ce68d647489a5e530d220afd833c
Henry Wilt <henrywwilt@gmail.com>
2022-10-04 16:01:24 -0400

    MD5_Part1 Complete

--- Shell_Scripting/MD5_Part1.sh
+++ Shell_Scripting/MD5_Part1.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
-find . -type 'f' -exec md5sum {} \; 2>/dev/null >> md5dict.txt
+# this script will find all files in the current directory and all subdirectories and calculate the MD5 checksum for each file 
+# and store the results in a file called md5dict.txt with the format of <filename><space><md5checksum>\n
+
+# The for loop is used to iterate through the md5sums.txt file 
+find . -type f -exec md5sum {} \; > md5sums.txt
+
+while read -r line
+do
+    #echo $line
+    HASH=`echo $line | cut -f1 -d ' '`
+    FILE=`echo $line | cut -f2 -d ' '`
+    #echo $HASH
+    #echo $FILE
+    #Put the md5sum and the file name into the dictionary in the format of "file md5sum"
+    echo "${FILE} ${HASH}" >> md5dict.txt 
+    
+done < md5sums.txt
 
 #echo test,tube | cut -f2 -d','
